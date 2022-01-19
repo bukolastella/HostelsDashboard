dda6e557f4619675629947af853d8398c0ac484e
Buks <abolarinbukolastella@gmail.com>
2022-01-19 15:48:21 +0100

    slider improved

--- details/index.js
+++ details/index.js
@@ -1,23 +1,56 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+"use strict";
+const slide = document.querySelectorAll(".slider_image");
+const btnRight = document.querySelector(".slider-btn-right");
+const btnLeft = document.querySelector(".slider-btn-left");
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-  console.log("klklk");
-}
+console.log(btnRight, btnLeft);
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slider_image");
-  console.log(x);
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "block";
-}
+const goToSlide = function (slides) {
+  console.log(slides);
+  slide.forEach((s, i) => {
+    if (100 * (i - slides) === 200) {
+      s.style.transform = `translateX( ${-100}%)`;
+    } else if (100 * (i - slides) === -200) {
+      s.style.transform = `translateX( ${100}%)`;
+    } else {
+      s.style.transform = `translateX( ${100 * (i - slides)}%)`;
+    }
+  });
+};
+
+const nextSlide = function () {
+  console.log("mmm");
+  if (curSlide === maxSlide - 1) curSlide = 0;
+  else curSlide++;
+  // curslide = 1{first slide}  we want -100% 0% 200% etc.
+  goToSlide(curSlide);
+  activeDots(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) curSlide = maxSlide - 1;
+  else curSlide--;
+  goToSlide(curSlide);
+  activeDots(curSlide);
+};
+
+//
+let curSlide = 0;
+const maxSlide = slide.length;
+goToSlide(0);
+activeDots(0);
+//dots
+// slide.forEach((s, i) =>
+//   dots.insertAdjacentHTML(
+//     "beforeend",
+//     `<button class="dot" data-slide="${i}"></button>`
+//   )
+// );
+// const activeDots = function (slide) {
+//   document
+//     .querySelectorAll(".dot")
+//     .forEach((d) => d.classList.remove("dot-active"));
+//   document
+//     .querySelector(`.dot[data-slide="${slide}"]`)
+//     .classList.add("dot-active");
+// };
